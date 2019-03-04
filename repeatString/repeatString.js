e28fddff84945bc823d50c44554a8e5f2a3282af
TUR7L3 <turtleplusplus@gmail.com>
2019-03-04 15:28:13 -0800

    Completed helloWorld, repeatString, and reverseString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let result = "";
+    if(num < 0){
+        return 'ERROR';
+    }
+    for(i = 0; i < num; i++){
+        result += str;
+    }
+    return result;
 }
 
 module.exports = repeatString
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    var result = "";
+    for (i=0; i < str.length; i++){
+        let char = str.length - i - 1;
+        result += str[char];    
+    }
+    return result;
 }
 
 module.exports = reverseString
