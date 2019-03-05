d4fce5c98c0cc044fce3aa81f22ca10839ffe255
TUR7L3 <turtleplusplus@gmail.com>
2019-03-04 21:39:04 -0800

    Completed leapYear

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,22 @@
-const leapYears = function() {
-
+const leapYears = function(arg) {
+    var year = Number(arg);
+    if (year%4 === 0){
+        if(year%100 === 0){
+            if(year%400 === 0)
+            {
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return true;
+        }
+    }
+    else{
+        return false;
+    }
 }
 
 module.exports = leapYears
