f9c49fde7c9b87ff2f6a7538a0f1d0f2b2bebf21
TUR7L3 <turtleplusplus@gmail.com>
2019-03-04 21:50:11 -0800

    Completed tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(arg) {
+  let result;
+  result = (arg - 32) * (5/9);
+  return Number(Math.round(result+'e1')+'e-1')
 }
 
-const ctof = function() {
-
+const ctof = function(arg) {
+  let result;
+  result = arg * (9/5) +32;
+  return Number(Math.round(result+'e1')+'e-1')
 }
 
 module.exports = {
