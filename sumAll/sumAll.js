760623917d114dbe930da51d2426a7d4350b3395
TUR7L3 <turtleplusplus@gmail.com>
2019-03-04 21:15:15 -0800

    Completed sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,15 +1,17 @@
-const sumAll = function(num1, num2) {
+const sumAll = function(arg1, arg2) {
     var result = 0;
-    if(num1 < 0 || num2 < 0){
-        return "ERROR."
+    var num1 = Number(arg1);
+    var num2 = Number(arg2);
+    if(num1 < 0 || num2 < 0 || typeof(arg1) != "number" || typeof(arg2) != "number") {
+        return "ERROR";
     }
     if(num1 < num2){
-        for(let i = num1; i == num2; i++){
+        for(let i = num1; i < num2 + 1; i++){
             result += i;
         }
     }
     else if(num1 > num2){
-        for(let i = num2; i == num1; i++){
+        for(let i = num2; i < num1 + 1; i++){
             result += i;
         }
     }
