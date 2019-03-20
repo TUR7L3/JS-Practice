363b2746b4d28948d667bf64d065f80edc61689e
TUR7L3 <turtleplusplus@gmail.com>
2019-03-20 11:42:01 -0700

    Completed Palindrome

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function() {
+const palindromes = function(phrase) {
+    let isPal;
+    let aphrase = phrase.replace(/[^\w\s]|_/g, "")
+                   .replace(/ /g, "");
+    aphrase = aphrase.toLowerCase();
+    let len = aphrase.length;
+    for(let i = 0; i < len; i++){
+        if(aphrase.charAt(i) == aphrase.charAt(len - i - 1)){
+            isPal = true;
+        }
+        else{
+            isPal = false;
+            return isPal;
+        }
+    }
+    return isPal;
 
 }
 
