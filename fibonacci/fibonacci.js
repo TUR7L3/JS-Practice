868fbf7110c11249bee51e89d2117d4c2041bbb4
TUR7L3 <turtleplusplus@gmail.com>
2019-04-01 16:59:12 -0700

    Completed Fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    let result = 0;
+    let fibArr = [];
+    fibArr[0] = 1;
+    fibArr[1] = 1;
+    let n = 2;
+    if(x < 0){
+        return 'OOPS';
+    }
+    for(let i = 0; i < x; i++){
+        fibArr[n] = fibArr[n-1] + fibArr[n-2];
+        n++;
+    }
+    return fibArr[x - 1];
+    
 }
 
 module.exports = fibonacci
