559d57c6758aeca0c6be81abfbe1570b1540f4e7
TUR7L3 <turtleplusplus@gmail.com>
2019-04-12 14:13:56 -0700

    Completed getTheTitle

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let titles = [];
 
+    for(i in books){
+        titles.push(books[i].title);
+    }
+    return titles;
 }
 
 module.exports = getTheTitles;
